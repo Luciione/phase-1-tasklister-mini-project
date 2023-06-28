1bb4ff175e0bc0ffe651cbc7c626ac73886791ec
+ <lucy.njoroge1@student.moringaschool.com>
2023-06-28 15:55:11 +0300

    Assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // Get references to the necessary elements
+const form = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+const deleteAllButton = document.querySelector('#delete-all-button');
+
+// Add event listener to the form
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get the task description from the input field
+  const taskInput = document.querySelector('#new-task-description');
+  const taskDescription = taskInput.value;
+
+  if (taskDescription !== '') {
+    // Create a new task item
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskDescription;
+
+    // Create the delete button for the task item
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'delete-button';
+
+    // Add event listener to the delete button
+    deleteButton.addEventListener('click', function() {
+      taskItem.remove(); // Remove the task item when clicked
+    });
+
+    // Append the delete button to the task item
+    taskItem.appendChild(deleteButton);
+
+    // Append the task item to the task list
+    taskList.appendChild(taskItem);
+
+    // Clear the input field
+    taskInput.value = '';
+  }
+});
+
+// Add event listener to the delete all button
+deleteAllButton.addEventListener('click', function() {
+  taskList.innerHTML = ''; // Clear all tasks
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
